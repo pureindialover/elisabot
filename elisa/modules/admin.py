a7d577d485edf84d0fc7c17a065574169389f1a4
pureindialover <67279597+pureindialover@users.noreply.github.com>
2020-06-25 12:40:32 +0530

    Update admin.py

--- elisa/modules/admin.py
+++ elisa/modules/admin.py
@@ -212,9 +212,10 @@ def invite(update, context):
                 if msg.chat.type == "private":
                         msg.reply_text("This command is meant to use in chat not in PM")
                         return ""
-        if user_can_invite(chat, user, context.bot.id) is False:
-    	msg.reply_text("You don't have enough rights to get invite link!")
-    	return ""
+                chat = update.effective_chat
+                if user_can_invite(chat, user, context.bot.id) is False:
+    	               msg.reply_text("You don't have enough rights to get invite link!")
+    	               return ""
         if chat.username:
                 msg.reply_text(chat.username)
         elif chat.type == chat.SUPERGROUP or chat.type == chat.CHANNEL:
