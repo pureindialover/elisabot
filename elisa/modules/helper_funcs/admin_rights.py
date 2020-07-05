bce0ad6093326c06db171569dc0cfea0b9a4a456
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-07-05 11:46:49 +0530

    Update admin_rights.py

--- elisa/modules/helper_funcs/admin_rights.py
+++ elisa/modules/helper_funcs/admin_rights.py
@@ -14,3 +14,6 @@ def user_can_invite(chat: Chat, user: User, bot_id: int) -> bool:
 
 def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
         return chat.get_member(user.id).can_change_info
+
+def user_can_delete(chat: Chat, user: User, bot_id: int) -> bool:
+        return chat.get_member(user.id).can_delete_messages
