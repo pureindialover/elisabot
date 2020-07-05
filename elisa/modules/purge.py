505ff257da3ad9eb77db437939bde029ac2a99ef
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-07-05 12:15:47 +0530

    Update purge.py

--- elisa/modules/purge.py
+++ elisa/modules/purge.py
@@ -9,6 +9,7 @@ from telegram.utils.helpers import mention_html
 
 from elisa import dispatcher, LOGGER
 from elisa.modules.helper_funcs.chat_status import user_admin, can_delete
+from elisa.modules.helper_funcs.admin_rights import user_can_delete
 from elisa.modules.log_channel import loggable
 
 
