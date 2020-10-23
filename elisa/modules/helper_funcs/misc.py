b49f8c168ba33d7cf2d55d140f7d743360cdc969
pureindialover <67279597+pureindialover@users.noreply.github.com>
2020-10-23 23:14:04 +0530

    Update misc.py

--- elisa/modules/helper_funcs/misc.py
+++ elisa/modules/helper_funcs/misc.py
@@ -123,6 +123,15 @@ def revert_buttons(buttons):
 def is_module_loaded(name):
     return (not LOAD or name in LOAD) and name not in NO_LOAD
 
+def build_keyboard_alternate(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
+        else:
+            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
+
+    return keyb
 
 def build_keyboard_parser(bot, chat_id, buttons):
     keyb = []
